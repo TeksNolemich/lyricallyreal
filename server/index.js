44eecd1f0c47246d26886775687e8ec7ad28bfb2
Arthur Tharp <atharp@uci.edu>
2018-12-02 21:12:40 -0800

    title modification

--- server/index.js
+++ server/index.js
@@ -6,4 +6,4 @@ const port = 2979;
 app.use(express.static(__dirname + '/../client/dist'));
 app.get('/search', calls.lyricSelector);
 app.get('/refresh_token', calls.refreshToken);
-app.listen(port, () => console.log(`listening on port: ${port}! hurrah`));
+app.listen(port, () => console.log(`listening on port: ${port}!`));
