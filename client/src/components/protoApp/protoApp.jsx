afb0b272cdc42ad597c7692b4b9216442c66c5c7
Arthur Tharp <atharp@uci.edu>
2018-12-07 20:48:29 -0800

    structure direct input submission

--- client/src/components/protoApp/protoApp.jsx
+++ client/src/components/protoApp/protoApp.jsx
@@ -6,24 +6,46 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
+      artistName: '',
       songName: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    console.log(event.target, ' the event');
+    this.setState({ [event.target.name]: event.target.value });
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.artistName, this.state.songName);
+  }
+
   render() {
     return (
-      <div className="search-container">
+      <div className="search-container-center">
         <label>ArtistName</label>
         <input
+          className="form-control
+          mr-sm-2"
           type="text"
           value={this.state.value}
+          name={'artistName'}
           onChange={this.handleChange}
         />
+        <label>SongName</label>
+        <input
+          className="form-control mr-sm-2"
+          type="text"
+          value={this.state.value}
+          name={'songName'}
+          onChange={this.handleChange}
+        />
+        <button type="Submit" onClick={this.handleSubmit}>
+          Hit IT
+        </button>
       </div>
     );
   }
